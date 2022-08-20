efb6d413ddd57eaef5da45071ed4d7a5049627e9
Juliana Nascimento <pessoal.juliana.n@gmail.com>
2022-08-20 20:03:09 -0300

    fecha menu ao mudar de página

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -2,8 +2,6 @@ import { DashboardComponent } from './dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
-
 const routes: Routes = [
   {
     path:'',
