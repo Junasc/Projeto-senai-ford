eeadeab01e4ae170ffc09cb36d5d148f5f6acf2c
Juliana Nascimento <pessoal.juliana.n@gmail.com>
2022-08-19 21:24:05 -0300

    adicionado função de pegar veículos

---
+++ src/app/api/vehicle.ts
@@ -0,0 +1,8 @@
+
+export interface vehicle {
+    id?: number;
+    vehicle?: string;
+    volumetotal?: number;
+    connected?: number;
+    softwareUpdates?: number;
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api/api.service';
+import { vehicle } from 'src/app/api/vehicle';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,12 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  vehicles: vehicle[] = [];
 
-  constructor(
-
-  ) { }
+  constructor(private apiService: ApiService) { 
+    this.getVehicles();
+  }
 
   ngOnInit(): void {
   }
 
+  getVehicles(): void {
+    this.apiService.getVehicles().subscribe((vehicles) => (this.vehicles = vehicles.vehicles));
+  }
 }
